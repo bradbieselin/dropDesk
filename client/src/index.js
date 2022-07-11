76d72b13783c22255aaca0c05eb1e132b47ecca8
Brad Bieselin <bradley.bieselin@gmail.com>
2022-07-11 19:03:47 -0400

    removed styled components

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
